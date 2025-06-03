62f0a9ff1e4db88f253fb14e32ef49f9ed1215df
Kay Chan <wwboy6@gmail.com>
2025-06-04 00:44:21 +0800

    feat: import smart router demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,168 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Native, ChainId, CurrencyAmount, TradeType, Percent } from '@pancakeswap/sdk'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SmartRouter, SmartRouterTrade, SMART_ROUTER_ADDRESSES, SwapRouter } from '@pancakeswap/smart-router'
+import { bscTokens } from '@pancakeswap/tokens'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import {
+  WagmiProvider,
+  createConfig,
+  useAccount,
+  useConnect,
+  useSwitchChain,
+  useChainId,
+  useSendTransaction,
+} from 'wagmi'
+import { bsc } from 'wagmi/chains'
 
-function App() {
+import { createPublicClient, hexToBigInt, http } from 'viem'
+import { GraphQLClient } from 'graphql-request'
+
+import './App.css'
+
+const chainId = ChainId.BSC
+const swapFrom = Native.onChain(chainId)
+// const swapTo = bscTokens.usdt
+const swapTo = bscTokens.cake
+const queryClient = new QueryClient()
+
+const publicClient = createPublicClient({
+  chain: bsc,
+  transport: http('https://bsc-dataseed1.binance.org'),
+  batch: {
+    multicall: {
+      batchSize: 1024 * 200,
+    },
+  },
+})
+
+export const config = createConfig({
+  chains: [bsc],
+  transports: {
+    [bsc.id]: http('https://bsc-dataseed1.binance.org'),
+  },
+})
+
+const v3SubgraphClient = new GraphQLClient('https://api.thegraph.com/subgraphs/name/pancakeswap/exchange-v3-bsc')
+const v2SubgraphClient = new GraphQLClient('https://proxy-worker-api.pancakeswap.com/bsc-exchange')
+
+const quoteProvider = SmartRouter.createQuoteProvider({
+  onChainProvider: () => publicClient,
+})
+
+function calculateGasMargin(value: bigint, margin = 1000n): bigint {
+  return (value * (10000n + margin)) / 10000n
+}
+
+export default function SmartRouterExample() {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <Main />
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+
+function Main() {
+  const currentChainId = useChainId()
+  const { address, isConnected } = useAccount()
+  const { connect, connectors } = useConnect()
+  const { switchChainAsync: switchNetwork } = useSwitchChain()
+  const { sendTransactionAsync } = useSendTransaction()
+
+  const [trade, setTrade] = useState<SmartRouterTrade<TradeType> | null>(null)
+  const amount = useMemo(() => CurrencyAmount.fromRawAmount(swapFrom, 10 ** 16), [])
+  const getBestRoute = useCallback(async () => {
+    const [v2Pools, v3Pools] = await Promise.all([
+      SmartRouter.getV2CandidatePools({
+        onChainProvider: () => publicClient,
+        v2SubgraphProvider: () => v2SubgraphClient,
+        v3SubgraphProvider: () => v3SubgraphClient,
+        currencyA: amount.currency,
+        currencyB: swapTo,
+      }),
+      SmartRouter.getV3CandidatePools({
+        onChainProvider: () => publicClient,
+        subgraphProvider: () => v3SubgraphClient,
+        currencyA: amount.currency,
+        currencyB: swapTo,
+        subgraphFallback: false,
+      }),
+    ])
+    const pools = [...v2Pools, ...v3Pools]
+    const trade = await SmartRouter.getBestTrade(amount, swapTo, TradeType.EXACT_INPUT, {
+      gasPriceWei: () => publicClient.getGasPrice(),
+      maxHops: 2,
+      maxSplits: 2,
+      poolProvider: SmartRouter.createStaticPoolProvider(pools),
+      quoteProvider,
+      quoterOptimization: true,
+    })
+    setTrade(trade)
+  }, [amount])
+
+  const swapCallParams = useMemo(() => {
+    if (!trade) {
+      return null
+    }
+    const { value, calldata } = SwapRouter.swapCallParameters(trade, {
+      recipient: address,
+      slippageTolerance: new Percent(1),
+    })
+    return {
+      address: SMART_ROUTER_ADDRESSES[chainId],
+      calldata,
+      value,
+    }
+  }, [trade, address])
+
+  const swap = useCallback(async () => {
+    if (!swapCallParams || !address) {
+      return
+    }
+
+    const { value, calldata, address: routerAddress } = swapCallParams
+
+    const tx = {
+      account: address,
+      to: routerAddress,
+      data: calldata,
+      value: hexToBigInt(value),
+    }
+    const gasEstimate = await publicClient.estimateGas(tx)
+    await sendTransactionAsync({
+      account: address,
+      chainId,
+      to: routerAddress,
+      data: calldata,
+      value: hexToBigInt(value),
+      gas: calculateGasMargin(gasEstimate),
+    })
+  }, [swapCallParams, address, sendTransactionAsync])
+
+  useEffect(() => {
+    if (isConnected && currentChainId !== chainId) {
+      switchNetwork?.({ chainId })
+    }
+  }, [isConnected, switchNetwork, currentChainId])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>Pancakeswap Smart Router Example.</p>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Get best quote swapping from {amount.toExact()} {amount.currency.symbol} to{' '}
+          {trade?.outputAmount.toExact() || '?'} {swapTo.symbol}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          {isConnected ? (
+            address
+          ) : (
+            <button onClick={() => connect({ connector: connectors[0] })}>Connect wallet</button>
+          )}
+        </p>
+        <p>{!trade ? <button onClick={getBestRoute}>Get Quote</button> : <button onClick={swap}>Swap</button>}</p>
       </header>
     </div>
-  );
+  )
 }
-
-export default App;
